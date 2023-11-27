d49fbda0f21208c11d9e462323aacd315034c4b1
mywus <109961342+mywus@users.noreply.github.com>
2023-11-27 15:56:47 +0200

    finished the Navbar Component, for mobile version and pc

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
+import { Navbar } from "./components"
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div><Navbar/></div>
   )
 }
